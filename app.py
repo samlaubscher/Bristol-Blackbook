0eec2346d075ecf97877c7b7f48130539cf5374b
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2020-10-11 02:33:55 +0100

    Add Session User profile

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from contextlib import redirect_stdout
 import os
 from flask import (
     Flask, flash, render_template, 
@@ -48,6 +49,8 @@ def register():
         # insert user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -64,18 +67,28 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("You are now logged in as {}".format(request.form.get("username")))
+                    flash("You are now logged in as {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # password incorrect
                 flash("You have entered an incorrect password and/or username")
                 return redirect(url_for("login"))
-
         else:
             # username does not exist
             flash("You have entered an incorrect password and/or username")
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # get session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
