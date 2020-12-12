ea23382fb348404a908b16dba20f6b7baa24abc1
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2020-12-12 19:52:49 +0000

    Implement hiding upload form not selected

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,13 @@
 window.onload = function(){
     // preloader
     document.querySelector(".loader").style.display = "none";
+    // new upload buttons
+    document.getElementById("artist-upload-btn").addEventListener('click', function() {
+        document.getElementById("crewPiece").classList.add("hidden");
+        document.getElementById("artistPiece").classList.remove("hidden");
+    });
+    document.getElementById("crew-upload-btn").addEventListener('click', function() {
+        document.getElementById("artistPiece").classList.add("hidden");
+        document.getElementById("crewPiece").classList.remove("hidden");
+    });
 };
