f665dc1d2ab01ba277fb05fe60356a70985b4ae5
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2020-12-14 00:17:50 +0000

    Update js comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,14 @@
 window.onload = function(){
     // preloader
     document.querySelector(".loader").style.display = "none";
-    // new upload buttons
+
+    // new upload page buttons
+    // artist
     document.getElementById("artist-upload-btn").addEventListener('click', function() {
         document.getElementById("crewPiece").classList.add("hidden");
         document.getElementById("artistPiece").classList.remove("hidden");
     });
+    // crew
     document.getElementById("crew-upload-btn").addEventListener('click', function() {
         document.getElementById("artistPiece").classList.add("hidden");
         document.getElementById("crewPiece").classList.remove("hidden");
